7bc04a6184876554c73537d925552424e169134f
肜浩 <13886509464@163.com>
2018-10-20 17:00:59 +0800

    新建User.java 到本地仓库

---
+++ src/main/java/com/ronghao/domain/User.java
@@ -0,0 +1,31 @@
+package com.ronghao.domain;
+
+public class User {
+    private String username;
+    private String age;
+    private String address;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
